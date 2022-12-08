faafdff321aeda9cbe8ea1c84695865b551be2ad
Joel Negrao <joelbisponeto@gmail.com>
2022-12-08 13:39:16 +0100

    Extend use case tests

--- app/vesting/use_cases/generate_schedule/use_case.py
+++ app/vesting/use_cases/generate_schedule/use_case.py
@@ -1,45 +1,72 @@
+"""
+Generate a schedule of vesting events for a given grant of options.
+"""
 from decimal import Decimal
 
 from dateutil.relativedelta import relativedelta
 from vesting.models import CompanyValuation, OptionGrant, Vest
+import datetime
 
 
 class GenerateScheduleUseCase:
+    """ Generate a schedule of vesting events for a given grant of options."""
 
-    def execute(self, option_grants: OptionGrant,
-                company_valuations: CompanyValuation):
-        """
-        Generate a schedule of vesting events for a given grant of options.
-        """
+    def execute(self, option_grants: OptionGrant, company_valuations: CompanyValuation):
+        """Execute the schedule generation."""
 
         return self._calculate_vests(option_grants, company_valuations)
 
-    def _calculate_vests(self, option_grants: OptionGrant,
-                         company_valuations: CompanyValuation):
+    def _calculate_vests(
+        self, option_grants: OptionGrant, company_valuations: CompanyValuation
+    ) -> list[Vest]:
         """
         Calculate the vesting schedule for a given grant of options.
         """
 
-        cliff = option_grants.cliff_months
-        duration = option_grants.duration_months
-        cliff_percentage = cliff / duration
+        duration: int = option_grants.duration_months
 
-        start_date = option_grants.start_date
+        if option_grants.cliff_months > duration:
+            raise ValueError("Cliff must be less than or equal to duration.")
 
-        vest_list = []
+        if option_grants.start_date < company_valuations.date:
+            raise ValueError(
+                "Start date must be greater than or equal to valuation date."
+            )
 
-        for index in range(0, duration+1):
+        vest_list: list[Vest] = []
 
-            current_date = start_date + \
-                relativedelta(months=+(index))
+        for index in range(0, duration + 1):
 
-            current_quantity = option_grants.quantity * ((
-                cliff_percentage +
-                ((index/duration) - cliff_percentage)
-            ) * (((duration-cliff) + index) // duration))
+            calculated_vest: Vest = self.calculate_vest_of_a_month(
+                option_grants.start_date,
+                option_grants.cliff_months,
+                duration,
+                option_grants.quantity,
+                company_valuations.price,
+                index,
+            )
+            vest_list.append(calculated_vest)
 
-            current_value = Decimal(current_quantity) * \
-                Decimal(company_valuations.price)
-            vest_list.append(Vest(vested_quantity=current_quantity,
-                             total_value=current_value, date=current_date))
         return vest_list
+
+    @staticmethod
+    def calculate_vest_of_a_month(
+        start_date: datetime.date,
+        cliff: int,
+        duration: int,
+        quantity: int,
+        price: Decimal,
+        month: int,
+    ) -> Vest:
+        """ Calculate the vesting of a month for a given grant of options."""
+        current_date: datetime.date = start_date + relativedelta(months=+(month))
+        cliff_percentage: float = cliff / duration
+
+        current_quantity: float = quantity * ((cliff_percentage + ((month / duration) - cliff_percentage))* (((duration - cliff) + month) // duration))  # noqa
+        current_value: Decimal = Decimal(current_quantity) * Decimal(price)
+
+        return Vest(
+            vested_quantity=current_quantity,
+            total_value=current_value,
+            date=current_date,
+        )
